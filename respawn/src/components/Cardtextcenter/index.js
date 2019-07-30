446e25c55962d5eee145887ef20e859e1769e584
Rodrigo Mady <rodrigo.mady@gmail.com>
2019-07-30 17:39:06 -0300

    [UPDATE] Add routes and bootstrap dependecies

--- respawn/src/components/Cardtextcenter/index.js
+++ respawn/src/components/Cardtextcenter/index.js
@@ -1,61 +1,61 @@
 import React, { Component } from "react";
 import "./style.css";
+// TODO: import images
+import meme from '../../images/meme.png';
 
 export default class Cardtextcenter extends Component {
   render() {
     return (
-      <body>
-        <div class="card text-center">
-        <h2>Paixão</h2>
-        <p>
-          Andar sobre duas rodas não é apenas um hobby, e sim
-          <strong>uma paixão</strong>
-        </p>
-        <img
-          src="/home/gabriel_rc/Imagens/velocimetro.jpeg"
-          alt="briefcase"
-          width="450"
-        />
-      </div>
-  
-      <div class="card text-center">
-        <h2>Gostos</h2>
-        <p>Não interessa a cilindrada, e sim <strong>a sua vontade</strong></p>
-  
-        <img
-          src="/home/gabriel_rc/Imagens/gostos.jpg"
-          alt="picture description"
-          width="450"
-        />
-      </div>
-  
-      <div class="card text-center">
-        <h2>Quebras de paradigma</h2>
-        <p>
-          Este motoclube deseja quebrar a máxima de
-          <strong>"todo motoqueiro cai e pilota sem respeitar os outros"</strong>
-        </p>
-  
-        <img
-          src="/home/gabriel_rc/Imagens/paradigma.jpg"
-          alt="picture description"
-          width="450"
-        />
-      </div>
-  
-      <div class="card text-center">
-        <h2>Amizade</h2>
-        <p>
-          Pois o que seria um motoclube
-          <strong>sem a amizade entre os membros deste?</strong>
-        </p>
-        <img
-          src="/home/gabriel_rc/Imagens/amizade.jpg"
-          alt="picture description"
-          width="450"
-        />
-      </div>
-      </body>
+        <body>
+        <div className="card text-center">
+          <h2>Paixão</h2>
+          <p>
+            Andar sobre duas rodas não é apenas um hobby, e sim
+            <strong>uma paixão</strong>
+          </p>
+          <img
+              src="/home/gabriel_rc/Imagens/velocimetro.jpeg"
+              alt="briefcase"
+              width="450"
+          />
+        </div>
+
+        <div className="card text-center">
+          <h2>Gostos</h2>
+          <p>Não interessa a cilindrada, e sim <strong>a sua vontade</strong></p>
+          <img
+              src="/home/gabriel_rc/Imagens/gostos.jpg"
+              alt="picture description"
+              width="450"
+          />
+        </div>
+
+        <div className="card text-center">
+          <h2>Quebras de paradigma</h2>
+          <p>
+            Este motoclube deseja quebrar a máxima de
+            <strong>"todo motoqueiro cai e pilota sem respeitar os outros"</strong>
+          </p>
+          <img
+              src="/home/gabriel_rc/Imagens/paradigma.jpg"
+              alt="picture description"
+              width="450"
+          />
+        </div>
+
+        <div className="card text-center">
+          <h2>Amizade</h2>
+          <p>
+            Pois o que seria um motoclube
+            <strong>sem a amizade entre os membros deste?</strong>
+          </p>
+          <img
+              src="{meme}"
+              alt="picture description"
+              width="450"
+          />
+        </div>
+        </body>
     );
   }
 }
