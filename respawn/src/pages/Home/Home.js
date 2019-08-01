e8d852715fb22a1cfdf741afb18baff2cbf488c7
gabrielgrc <gabriel.bsb98@gmail.com>
2019-08-01 20:20:37 -0300

    componente footer

--- respawn/src/pages/Home/Home.js
+++ respawn/src/pages/Home/Home.js
@@ -71,32 +71,8 @@ export default class Home extends Component {
             </p>
             <img src={amizade} alt="picture description" width="450" />
           </div>
+          <Footer />
           
-          <div id="footer">
-            <p>Mídias sociais do ©Respawn Club</p>
-            <ul class="list-inline">
-              <li>
-                <a href="#">
-                  <i class="fab fa-youtube" />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fab fa-facebook" />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fab fa-twitter" />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fab fa-instagram" />
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
     );
