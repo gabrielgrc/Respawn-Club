eaa5fc573e8442df9aeee27b7ab8b913ef9c5584
gabrielgrc <gabriel.bsb98@gmail.com>
2019-08-01 19:51:29 -0300

    css

--- respawn/src/pages/Home/Home.js
+++ respawn/src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import PropTypes from "prop-types";
 
 import Banner from "../../components/Banner/index";
+import Footer from "../../components/Footer";
 //import meme from '../../images/meme.png';
-import velocimetro from "../../images/velocimetro.jpeg"
-import gostos from "../../images/gostos.jpg"
-import paradigma from "../../images/paradigma.jpg"
-import amizade from "../../images/amizade.jpg"
+import velocimetro from "../../images/velocimetro.jpeg";
+import gostos from "../../images/gostos.jpg";
+import paradigma from "../../images/paradigma.jpg";
+import amizade from "../../images/amizade.jpg";
 
 import Service from "./Service";
 
@@ -26,64 +27,77 @@ export default class Home extends Component {
     };
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   render() {
     const { data } = this.state;
     return (
       <div>
-          <Banner />
-          <div className="content">
-            <div className="card text-center">
-              <h2>Paixão</h2>
-              <p>
-                Andar sobre duas rodas não é apenas um hobby, e sim
-                <strong>uma paixão</strong>
-              </p>
-              <img
-                  src={velocimetro}
-                  alt="picture description"
-                  width="450"
-              />
-            </div>
-
-            <div className="card text-center">
-              <h2>Gostos</h2>
-              <p>Não interessa a cilindrada, e sim <strong>a sua vontade</strong></p>
-              <img
-                  src={gostos}
-                  alt="picture description"
-                  width="450"
-              />
-            </div>
-
-            <div className="card text-center">
-              <h2>Quebras de paradigma</h2>
-              <p>
-                Este motoclube deseja quebrar a máxima de
-                <strong>"todo motoqueiro cai e pilota sem respeitar os outros"</strong>
-              </p>
-              <img
-                  src={paradigma}
-                  alt="picture description"
-                  width="450"
-              />
-            </div>
-
-            <div className="card text-center">
-              <h2>Amizade</h2>
-              <p>
-                Pois o que seria um motoclube
-                <strong>sem a amizade entre os membros deste?</strong>
-              </p>
-              <img
-                  src={amizade}
-                  alt="picture description"
-                  width="450"
-              />
-            </div>
+        <Banner />
+        <div className="content">
+          <div className="card text-center">
+            <h2>Paixão</h2>
+            <p>
+              Andar sobre duas rodas não é apenas um hobby, e sim
+              <strong>uma paixão</strong>
+            </p>
+            <img src={velocimetro} alt="picture description" width="450" />
           </div>
+
+          <div className="card text-center">
+            <h2>Gostos</h2>
+            <p>
+              Não interessa a cilindrada, e sim <strong>a sua vontade</strong>
+            </p>
+            <img src={gostos} alt="picture description" width="450" />
+          </div>
+
+          <div className="card text-center">
+            <h2>Quebras de paradigma</h2>
+            <p>
+              Este motoclube deseja quebrar a máxima de
+              <strong>
+                "todo motoqueiro cai e pilota sem respeitar os outros"
+              </strong>
+            </p>
+            <img src={paradigma} alt="picture description" width="450" />
+          </div>
+
+          <div className="card text-center">
+            <h2>Amizade</h2>
+            <p>
+              Pois o que seria um motoclube
+              <strong>sem a amizade entre os membros deste?</strong>
+            </p>
+            <img src={amizade} alt="picture description" width="450" />
+          </div>
+          
+          <div id="footer">
+            <p>Mídias sociais do ©Respawn Club</p>
+            <ul class="list-inline">
+              <li>
+                <a href="#">
+                  <i class="fab fa-youtube" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-facebook" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-twitter" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-instagram" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
